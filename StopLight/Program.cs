db0ffc3101abe8d5bc7218ccf7882eefd68cb708
Mitchell Combs <mitch.combs.92@gmail.com>
2018-06-04 13:37:10 -0400

    Initial Commit

--- StopLight/Program.cs
+++ StopLight/Program.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace StopLight {
 	class Program {
+
+		enum TrafficLight {
+			Red = 1, Green, Yellow
+		};
+
+		void Displaytrafficlight(TrafficLight Color) {
+			switch(Color) {
+				case TrafficLight.Red:
+					Console.WriteLine("Red");
+					break;
+				case TrafficLight.Green:
+					Console.WriteLine("Green");
+					break;
+				case TrafficLight.Yellow:
+					Console.WriteLine("Yellow");
+					break;
+			}
+		}
+
+		void run() {
+			for (int idx = 0; idx < 3; idx++) {
+				int coloridx = 1; // color is red
+				TrafficLight color = (TrafficLight) coloridx;
+				Displaytrafficlight(color);
+				color = (TrafficLight)(++coloridx);
+				Displaytrafficlight(color);
+				color = (TrafficLight)(++coloridx);
+				Displaytrafficlight(color);
+			}
+		}
+
 		static void Main(string[] args) {
+
+			(new Program()).run();
+
 		}
 	}
 }
